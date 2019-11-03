68209e0b7e804cffaec71609dcf648b872bcf9d9
Amin <yazdi.mohammad99@gmail.com>
2019-11-04 00:34:41 +0330

    Add equals to class

--- src/algorithms/search/Node.java
+++ src/algorithms/search/Node.java
@@ -4,7 +4,20 @@ public class Node {
     int[][] grid;
     int blankX, blankY;
 
-    public Node(int Size) {
-        grid = new int[Size][Size];
+    public Node(int[][] puzzle) {
+        this.grid = puzzle;
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        Node that = (Node) obj;
+        if (this.blankX != that.blankX || this.blankY != that.blankY)
+            return false;
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                if (this.grid[i][j] != that.grid[i][j])
+                    return false;
+        return true;
+
     }
 }
