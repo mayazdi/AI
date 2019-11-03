d23ffba867feacc91c7e08f64fe41f6ac9824b8b
Amin <yazdi.mohammad99@gmail.com>
2019-11-03 23:29:07 +0330

    Minor Edit

--- src/algorithms/search/BFS.java
+++ src/algorithms/search/BFS.java
@@ -5,6 +5,10 @@ import algorithms.Algorithm;
 public class BFS implements Algorithm {
     @Override
     public String makeMove(String[][] grid) {
+        //Turn Initial State to my structure
+        //Call BFS on initial state
+        //
+
         return null;
     }
 }
--- src/algorithms/search/Util.java
+++ src/algorithms/search/Util.java
@@ -1,17 +1,11 @@
 package algorithms.search;
 
+import java.util.ArrayList;
+
 enum Direction {
     Right, Left, Up, Down
 }
 
-class Node {
-    int[][] grid;
-    int blankX, blankY;
-
-    public Node(int Size) {
-        grid = new int[Size][Size];
-    }
-}
 
 public class Util {
     //goal State
@@ -21,36 +15,38 @@ public class Util {
         Goal = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 0}};
     }
 
-    public void NextStates(Node node) {
+    public ArrayList<Node> NextStates(Node node) {
+        ArrayList<Node> available = new ArrayList<>();
         Node n1, n2, n3, n4;
         if (isValid(node, Direction.Down)) {
             n1 = node;
             n1.grid[n1.blankX][n1.blankY] = n1.grid[n1.blankX][n1.blankY + 1];
             n1.blankY = n1.blankY + 1;
             n1.grid[n1.blankX][n1.blankY] = 0;
+            available.add(n1);
         }
         if (isValid(node, Direction.Up)) {
             n2 = node;
             n2.grid[n2.blankX][n2.blankY] = n2.grid[n2.blankX][n2.blankY - 1];
             n2.blankY = n2.blankY - 1;
             n2.grid[n2.blankX][n2.blankY] = 0;
-
+            available.add(n2);
         }
         if (isValid(node, Direction.Right)) {
             n3 = node;
             n3.grid[n3.blankX][n3.blankY] = n3.grid[n3.blankX + 1][n3.blankY];
             n3.blankX = n3.blankX + 1;
             n3.grid[n3.blankX][n3.blankY] = 0;
-
+            available.add(n3);
         }
         if (isValid(node, Direction.Left)) {
             n4 = node;
             n4.grid[n4.blankX][n4.blankY] = n4.grid[n4.blankX - 1][n4.blankY];
             n4.blankX = n4.blankX - 1;
             n4.grid[n4.blankX][n4.blankY] = 0;
+            available.add(n4);
         }
-
-
+        return available;
     }
 
     private boolean isValid(Node node, Direction direction) {
