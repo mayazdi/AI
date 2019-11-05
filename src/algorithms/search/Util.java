5eec94fb16d896c2120b054c918bf3a70bbf5d94
Amin <yazdi.mohammad99@gmail.com>
2019-11-05 18:38:18 +0330

    Cleaning!

--- src/algorithms/search/Util.java
+++ src/algorithms/search/Util.java
@@ -2,7 +2,6 @@ package algorithms.search;
 
 import java.util.ArrayList;
 
-
 public class Util {
     static Node Goal;
     int Final[][];
@@ -55,60 +54,29 @@ public class Util {
         return moved;
     }
 
-
     public static ArrayList<Node> NextStates(Node node) {
         ArrayList<Node> available = new ArrayList<>();
         Node n1, n2, n3, n4;
         if (isValid(node, Direction.Down)) {
             n1 = move(node, Direction.Down);
-            /*n1 = new Node(node.grid, node.blankX, node.blankY);
-            System.out.println(n1.blankX + " " + n1.blankY);
-            n1.parent = node;
-            n1.move = Direction.Down;
-            n1.grid[n1.blankX][n1.blankY] = n1.grid[n1.blankX][n1.blankY + 1];
-            n1.blankY = n1.blankY + 1;
-            n1.grid[n1.blankX][n1.blankY] = 0;*/
             available.add(n1);
             n1.toString();
             System.out.println("Down");
         }
         if (isValid(node, Direction.Up)) {
             n2 = move(node, Direction.Up);
-            /*n2 = new Node(node.grid, node.blankX, node.blankY);
-            System.out.println(n2.blankX + " " + n2.blankY);
-            n2.parent = node;
-            n2.move = Direction.Down;
-            n2.grid[n2.blankX][n2.blankY] = n2.grid[n2.blankX][n2.blankY - 1];
-            n2.blankY = n2.blankY - 1;
-            n2.grid[n2.blankX][n2.blankY] = 0;*/
             available.add(n2);
             n2.toString();
             System.out.println("Up");
-
-            // Does what Left Must Do
         }
         if (isValid(node, Direction.Right)) {
             n3 = move(node, Direction.Right);
-            /*n3 = new Node(node.grid, node.blankX, node.blankY);
-            System.out.println(n3.blankX + " " + n3.blankY);
-            n3.parent = node;
-            n3.move = Direction.Down;
-            n3.grid[n3.blankX][n3.blankY] = n3.grid[n3.blankX + 1][n3.blankY];
-            n3.blankX = n3.blankX + 1;
-            n3.grid[n3.blankX][n3.blankY] = 0;*/
             available.add(n3);
             n3.toString();
             System.out.println("Right");
         }
         if (isValid(node, Direction.Left)) {
             n4 = move(node, Direction.Left);
-            /*n4 = new Node(node.grid, node.blankX, node.blankY);
-            System.out.println(n4.blankX + " " + n4.blankY);
-            n4.parent = node;
-            n4.move = Direction.Down;
-            n4.grid[n4.blankX][n4.blankY] = n4.grid[n4.blankX - 1][n4.blankY];
-            n4.blankX = n4.blankX - 1;
-            n4.grid[n4.blankX][n4.blankY] = 0;*/
             available.add(n4);
             n4.toString();
             System.out.println("Left");
